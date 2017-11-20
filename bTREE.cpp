97921a8b173913b70a8f6dcc5bfc9980dcb20c31
JarekBrown <JarekBrown@users.noreply.github.com>
2017-11-20 16:33:49 -0700

    worked on insert method

--- bTREE.cpp
+++ bTREE.cpp
@@ -1,33 +1,47 @@
 #include "bTREE.h"
+#include <iostream>
 
 
 //look at descriptions in pMT.h for guidance on what you might need for these function to actually do
 bTREE::bTREE()
 {
+	nodeCount = 0;
+	tree = new list<treeNode>;
 }
 
 bTREE::~bTREE()
 {
+	delete tree;
 }
 
 int bTREE::dataInserted()
 {
+	return 0;
 }
 
 int bTREE::numberOfNodes()
 {
+	return nodeCount;
 }
 
-int bTREE::insert(string data, int time)
+bool bTREE::insert(string data, int time)
 {
+	treeNode temp;
+	temp.data = data;
+	temp.time = time;
+	tree->insert(temp);
+	nodeCount++;
+	return true;
 }
 
-int bTREE::find(string)
+bool bTREE::find(string)
 {
+	return false;
 }
 
-string bTREE::locate()
+string bTREE::locate(string target)
 {
+	return "";
 }
 
 friend bool bTREE::operator ==(const bTREE& lhs, const bTREE& rhs)
@@ -44,4 +58,5 @@ friend bool bTREE::operator !=(const bTREE& lhs, const bTREE& rhs)
 
 friend std::ostream& bTREE::operator <<(std::ostream& out, const bTREE& p)
 {
+	
 }
