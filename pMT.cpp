760851bfbf06153d7f2946f999afef45bb6e121e
JarekBrown <jab201506@gmail.com>
2017-12-01 23:49:26 -0700

    minor edits

--- pMT.cpp
+++ pMT.cpp
@@ -19,8 +19,8 @@ pMT::pMT(int hashSelect, int size)
 {
 	selectedHash = hashSelect;
 	pmtSize = size;
-	hashList = new string[size]; //array of all hashes
-	itemList = new pair<string, int>[size]; //list of all values
+	//hashList = new string[size]; //array of all hashes
+	//itemList = new pair<string, int>[size]; //list of all values
 	for(int i = 0; i < size*2; i++) //need to create 2x the number desired leaves to get correct build
 	{
 		insert("", 0.0);
