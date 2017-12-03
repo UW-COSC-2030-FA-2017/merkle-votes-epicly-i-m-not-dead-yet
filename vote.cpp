fe8526492d0c85b5ed3721e461733fa3c71c5a36
JarekBrown <jab201506@gmail.com>
2017-12-02 17:58:11 -0700

    started work on file

--- vote.cpp
+++ vote.cpp
@@ -1,9 +1,45 @@
 #include <stdio.h>
-#include "pMT.h"
+#include <fstream>
+#include "pMT.cpp"
 using namespace std;
 
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
+	string filename;
+	ifstream infile;
+	pMT mTree;
+	int hashSelect;
+	
+	cout << "enter file name: ";
+	cin >> filename;
+	
+	cout << "which hash function (1/2/3): ";
+	cin >> hashSelect;
+	
+	infile.open(filename);
+	
+	if(filename.is_open)
+	{
+		int size = 0;
+		while(!infile.eof())
+		{
+			size++;
+		}
+		mTree = new pMT(hashSelect, size);
+		while(!infile.eof())
+		{
+			mTree.insert("", 3);
+		}
+		
+		mTree.hashItAllOut();
+		cout << mTree;
+	}
+	
+	else{
+		cout << "unable to open file" << endl;
+	}
+	
+	infile.close();
+	
 	return 0;
 }
